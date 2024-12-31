2c61e4caaa734615c0dee369b41442be437afc3d
bruj-al-arob <Nishanpatowary53@gmail.com>
2024-12-31 00:15:05 -0800

    first commit

---
+++ src/componants/Contruct/Contruct.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contruct = () => {
+    return (
+        <div>
+            <h1>contruct</h1>
+            
+            
+            
+        </div>
+    );
+};
+
+export default Contruct;
